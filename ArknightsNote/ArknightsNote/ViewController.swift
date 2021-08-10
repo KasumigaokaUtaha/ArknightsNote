28c0602f5642ae0eb1d8672f81cc86ad64b9f404
KasumigaokaUtaha <blacklotussaki@gmail.com>
2021-08-10 21:54:32 +0200

    Init commit

---
+++ ArknightsNote/ArknightsNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArknightsNote
+//
+//  Created by Kasumigaoka Utaha on 10.08.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
