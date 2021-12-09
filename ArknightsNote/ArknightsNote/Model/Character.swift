9ce5f3bea546db843d9c09abbca8b149c91f9287
KasumigaokaUtaha <blacklotussaki@gmail.com>
2021-12-09 16:52:25 +0100

    Update and move character_table.json to assets && update character model && update initializer of CharacterStore

--- ArknightsNote/ArknightsNote/Model/Character.swift
+++ ArknightsNote/ArknightsNote/Model/Character.swift
@@ -7,80 +7,149 @@
 
 import Foundation
 
-struct Character: Codable, Hashable {
+struct Character: Codable, Equatable, Hashable {
+    // MARK: - Protocol implementation
+    static func == (lhs: Character, rhs: Character) -> Bool {
+        return lhs.name == rhs.name
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(self.name)
+    }
+    
+    // MARK: - Property
     var name: String
     var description: String
-//    var canUseGeneralPotentialItem: Bool
-//    var potentialItemId: String
-//    var nationId: String
-//    var groupId: String
-//    var teamId: String
-//    var displayNumber: String
-//    var tokenKey: String
-//    var appellation: String
+    var canUseGeneralPotentialItem: Bool
+    var potentialItemId: String
+    var nationId: String?
+    var groupId: String?
+    var teamId: String?
+    var displayNumber: String?
+    var tokenKey: String?
+    var appellation: String
     var position: String
     var tagList: [String]
-    var itemUsage: String
-    var itemDesc: String
-    var itemObtainApproach: String
+    var itemUsage: String?
+    var itemDesc: String?
+    var itemObtainApproach: String?
     var isNotObtainable: Bool
-//    var isSpChar: Bool
-//    var maxPotentialLevel: Int
+    var isSpChar: Bool
+    var maxPotentialLevel: Int
     var rarity: Int
     var profession: String
-//    var trait: String
-//    var phases: [Phase]
-//    var skills: [Skill]
-//    var talents: [Talent]
+    var subProfessionId: String
+    var trait: Trait?
+    var phases: [Phase]
+    var skills: [Skill]
+    var talents: [Talent]?
 //    var potentialRanks: []
 //    var favorKeyFrames: []
 //    var allSkillLvup: []
     
-//    struct Phase: Codable {
-//        var characterPrefabKey: String
-//        var rangeId: String
-//        var maxLevel: Int
-//        var attributesKeyFrames: [AttributesKeyFrames]
-//
-//        struct AttributesKeyFrames: Codable {
-//            var level: Int
-//            var data: Data
-//
-//            struct Data: Codable {
-//                var maxHp: Int
-//                var atk: Int
-//                var def: Int
-//                var magicResistance: Double
-//                var cost: Int
-//                var blockCnt: Int
-//                var moveSpeed: Double
-//                var attackSpeed: Double
-//                var baseAttackTime: Double
-//                var respawnTime: Int
-//                var hpRecoveryPerSec: Double
-//                var spRecoveryPerSec: Double
-//                var maxDeployCount: Int
-//                var maxDeckStackCnt: Int
-//                var tauntLevel: Int
-//                var massLevel: Int
-//                var baseForceLevel: Int
-//                var stunImmune: Bool
-//                var silenceImmune: Bool
-//                var sleepImmune: Bool
-//            }
-//        }
-//    }
+    struct Trait: Codable {
+        var candidates: [Candidate]
+        
+        struct Candidate: Codable {
+            var unlockCondition: UnlockCondition
+            var requiredPotentialRank: Int
+            var blackboard: [Blackboard]
+            var overrideDescription: String?
+            var prefabKey: String?
+            var rangeId: String?
+
+            struct UnlockCondition: Codable {
+                var phase: Int
+                var level: Int
+            }
+            
+            struct Blackboard: Codable {
+                var key: String
+                var value: Double
+            }
+        }
+    }
     
-//    struct Skill: Codable {
-//        var skillId: String
-//        var overridePrefabKey: String
-//        var overrideTokenKey: String
-////        var levelUpCostCond: [String]
-//        var unlockCond: UnlockCond
-//
-//        struct UnlockCond: Codable {
-//            var phase: Int
-//            var level: Int
-//        }
-//    }
+    struct Phase: Codable {
+        var characterPrefabKey: String
+        var rangeId: String
+        var maxLevel: Int
+        var attributesKeyFrames: [AttributesKeyFrames]
+
+        struct AttributesKeyFrames: Codable {
+            var level: Int
+            var data: Data
+
+            struct Data: Codable {
+                var maxHp: Int
+                var atk: Int
+                var def: Int
+                var magicResistance: Double
+                var cost: Int
+                var blockCnt: Int
+                var moveSpeed: Double
+                var attackSpeed: Double
+                var baseAttackTime: Double
+                var respawnTime: Int
+                var hpRecoveryPerSec: Double
+                var spRecoveryPerSec: Double
+                var maxDeployCount: Int
+                var maxDeckStackCnt: Int
+                var tauntLevel: Int
+                var massLevel: Int
+                var baseForceLevel: Int
+                var stunImmune: Bool
+                var silenceImmune: Bool
+                var sleepImmune: Bool
+            }
+        }
+    }
+    
+    struct Skill: Codable {
+        var skillId: String
+        var overridePrefabKey: String?
+        var overrideTokenKey: String?
+        var levelUpCostCond: [LevelUpCostCond]
+        
+        struct LevelUpCostCond: Codable {
+            var unlockCond: UnlockCond
+            var lvlUpTime: Int
+            var levelUpCost: [LevelUpCost]?
+            
+            struct UnlockCond: Codable {
+                var phase: Int
+                var level: Int
+            }
+            
+            struct LevelUpCost: Codable {
+                var id: String
+                var count: Int
+                var type: String
+            }
+        }
+    }
+    
+    struct Talent: Codable {
+        var candidates: [Candidate]
+        
+        struct Candidate: Codable {
+            var unlockCondition: UnlockCondition
+            var requiredPotentialRank: Int
+            var prefabKey: String
+            var name: String
+            var description: String
+            var rangeId: String?
+            var blackboard: [Blackboard]
+
+            struct UnlockCondition: Codable {
+                var phase: Int
+                var level: Int
+            }
+            
+            struct Blackboard: Codable {
+                var key: String
+                var value: Double
+            }
+        }
+    }
 }
--- ArknightsNote/ArknightsNote/Model/CharacterStore.swift
+++ ArknightsNote/ArknightsNote/Model/CharacterStore.swift
@@ -5,6 +5,7 @@
 //  Created by Kasumigaoka Utaha on 14.08.21.
 //
 
+import UIKit
 import Foundation
 
 struct CharacterStore {
@@ -12,28 +13,37 @@ struct CharacterStore {
     private var characters: [Character]
 
     init() {
-        let url = Bundle.main.url(forResource: "character_table", withExtension: "json")
-        guard let url = url else {
-            fatalError("Could not find the resource file character_table.json")
-        }
-        
         characters = []
+        guard let asset = NSDataAsset(name: "character_table.json") else {
+            fatalError("Missing data asset: character_table.json")
+        }
+        guard let characterTables = try? JSONSerialization.jsonObject(with: asset.data, options: .mutableContainers) as? [String:Any] else {
+            fatalError("Could not parse contents of data asset: character_table.json as json")
+        }
+        
         let decoder = JSONDecoder()
-        guard let data = try? Data(contentsOf: url) else {
-            fatalError("Could not load contents of \(url)")
-        }
-        
-        guard let char_dict = try? JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String:Any] else {
-            fatalError("Could not parse contents of \(url) as json")
-        }
-        
-        for value in char_dict.values {
-            guard let json_value = try? JSONSerialization.data(withJSONObject: value) else {
+        for key in characterTables.keys {
+            guard key.hasPrefix("char") else { continue }
+            guard let data = try? JSONSerialization.data(withJSONObject: characterTables[key]!) else {
                 fatalError("Could not convert parsed data into JSON Object")
             }
             
-            if let char = try? decoder.decode(Character.self, from: json_value) {
-                characters.append(char)
+            do {
+                let character = try decoder.decode(Character.self, from: data)
+                characters.append(character)
+            } catch let DecodingError.dataCorrupted(context) {
+                print(context)
+            } catch let DecodingError.keyNotFound(key, context) {
+                print("Key '\(key)' not found:", context.debugDescription)
+                print("codingPath:", context.codingPath)
+            } catch let DecodingError.valueNotFound(value, context) {
+                print("Value '\(value)' not found:", context.debugDescription)
+                print("codingPath:", context.codingPath)
+            } catch let DecodingError.typeMismatch(type, context)  {
+                print("Type '\(type)' mismatch:", context.debugDescription)
+                print("codingPath:", context.codingPath)
+            } catch {
+                print("error: ", error)
             }
         }
     }
